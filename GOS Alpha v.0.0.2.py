6cade938960997e833f1f8ecd0bfbae6cb93bb5b
Glitchblox <81377058+Glitchblox@users.noreply.github.com>
2021-03-25 18:33:04 -0500

    Add files via upload

---
+++ GOS Alpha v.0.0.2.py
@@ -0,0 +1,217 @@
+import time
+
+import sys
+
+print("Booting Up Glitch Operation system")
+time.sleep(0.5)
+print("Software Succesfully booted up!")
+time.sleep(0.5)
+print("Input Username")
+
+username = input()
+if username == "New User":
+  print("Input Password")
+  password = input()
+  print("Accepted!")
+  cmd = input("GOScmd > ")
+  while cmd != "shutdownupupdowndownleftrightleftrightBAinthatexactorder":
+    if cmd == "beans" or cmd == "Beans" or cmd == "BEANS":
+      print("""
+  ######  #######    #    #     #  #####  
+  #     # #         # #   ##    # #     # 
+  #     # #        #   #  # #   # #       
+  ######  #####   #     # #  #  #  #####  
+  #     # #       ####### #   # #       # 
+  #     # #       #     # #    ## #     # 
+  ######  ####### #     # #     #  #####  """)
+      cmd = input("GOScmd > ")
+
+    if cmd == "shotgun" or cmd == "Shotgun" or cmd =="SHOTGUN":
+     print("POW POW POW")
+     cmd = input("GOScmd > ")
+
+    if cmd == "files":
+      print("""dog.txt
+       (type esc twice to exit)""")
+      files = "null"
+      while files != "esc":
+        files = input("GOS\Files > ")
+        if files == "dog.txt":
+         print("""
+             __
+       (___()'`;
+      /,    /`
+      \\"--\\""")
+        files=input("GOS\Files > ")
+      
+        if files == "esc":
+          print("exiting files...")
+          time.sleep(0.5)
+          print("                    ")
+          cmd = input("GOScmd > ")
+
+    if cmd == "shutdown" or cmd == "Shutdown" or cmd == "SHUTDOWN":
+      print("Shutting down...")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      cmd = "shutdownupupdowndownleftrightleftrightBAinthatexactorder"
+    if cmd == "BetaAI":
+      print("""
+            |       |
+          \___________/
+          
+          Hello...""")
+      time.sleep(2)
+      print("Shutting down...")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      cmd = "shutdownupupdowndownleftrightleftrightBAinthatexactorder"
+  
+if username == "Glitchblox":
+  print("Input Password")
+  password = input()
+  if password == "comma":
+    print("Accepted but in lowercase!")
+    cmd = input("GOScmd > ")
+    while cmd != "shutdownupupdowndownleftrightleftrightBAinthatexactorder":
+      if cmd == "beans" or cmd == "Beans" or cmd == "BEANS":
+        print("""
+  ######  #######    #    #     #  #####  
+  #     # #         # #   ##    # #     # 
+  #     # #        #   #  # #   # #       
+  ######  #####   #     # #  #  #  #####  
+  #     # #       ####### #   # #       # 
+  #     # #       #     # #    ## #     # 
+  ######  ####### #     # #     #  #####  """)
+        cmd = input("GOScmd > ")
+
+      if cmd == "shotgun" or cmd == "Shotgun" or cmd =="SHOTGUN":
+       print("POW POW POW")
+       cmd = input("GOScmd > ")
+
+      if cmd == "files":
+        print("""dog.txt
+         (type esc twice to exit)""")
+        files = "null"
+        while files != "esc":
+          files = input("GOS\Files > ")
+          if files == "dog.txt":
+           print("""
+               __
+         (___()'`;
+        /,    /`
+        \\"--\\""")
+          files=input("GOS\Files > ")
+        
+          if files == "esc":
+            print("exiting files...")
+            time.sleep(0.5)
+            print("                    ")
+            cmd = input("GOScmd > ")
+
+      if cmd == "shutdown" or cmd == "Shutdown" or cmd == "SHUTDOWN":
+        print("Shutting down...")
+        time.sleep(0.5)
+        print(".")
+        time.sleep(0.5)
+        print(".")
+        time.sleep(0.5)
+        print(".")
+        time.sleep(0.5)
+        cmd = "shutdownupupdowndownleftrightleftrightBAinthatexactorder"
+      if cmd == "BetaAI":
+        print("""
+              |       |
+            \___________/
+            
+            Hello...""")
+        time.sleep(2)
+        print("Shutting down...")
+        time.sleep(0.5)
+        print(".")
+        time.sleep(0.5)
+        print(".")
+        time.sleep(0.5)
+        print(".")
+        time.sleep(0.5)
+        cmd = "shutdownupupdowndownleftrightleftrightBAinthatexactorder"
+
+
+if username !="New User" and username != "Glitchblox":
+  print("Input Password")
+  password = input()
+  print("Accepted!")
+  cmd = input("GOScmd > ")
+  while cmd != "shutdownupupdowndownleftrightleftrightBAinthatexactorder":
+    if cmd == "beans" or cmd == "Beans" or cmd == "BEANS":
+      print("""
+  ######  #######    #    #     #  #####  
+  #     # #         # #   ##    # #     # 
+  #     # #        #   #  # #   # #       
+  ######  #####   #     # #  #  #  #####  
+  #     # #       ####### #   # #       # 
+  #     # #       #     # #    ## #     # 
+  ######  ####### #     # #     #  #####  """)
+      cmd = input("GOScmd > ")
+
+    if cmd == "shotgun" or cmd == "Shotgun" or cmd =="SHOTGUN":
+     print("POW POW POW")
+     cmd = input("GOScmd > ")
+
+    if cmd == "files":
+      print("""dog.txt
+       (type esc twice to exit)""")
+      files = "null"
+      while files != "esc":
+        files = input("GOS\Files > ")
+        if files == "dog.txt":
+         print("""
+             __
+       (___()'`;
+      /,    /`
+      \\"--\\""")
+        files=input("GOS\Files > ")
+      
+        if files == "esc":
+          print("exiting files...")
+          time.sleep(0.5)
+          print("                    ")
+          cmd = input("GOScmd > ")
+
+    if cmd == "shutdown" or cmd == "Shutdown" or cmd == "SHUTDOWN":
+      print("Shutting down...")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      cmd = "shutdownupupdowndownleftrightleftrightBAinthatexactorder"
+    if cmd == "BetaAI":
+      print("""
+            |       |
+          \___________/
+          
+          Hello...""")
+      time.sleep(2)
+      print("Shutting down...")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      print(".")
+      time.sleep(0.5)
+      cmd = "shutdownupupdowndownleftrightleftrightBAinthatexactorder"
+  
